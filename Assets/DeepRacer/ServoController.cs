2c57e93e35ee881100f1bccf6ce1ece25fdac09f
Geoffrey Mantel <geoffrey.mantel@gmail.com>
2022-01-05 18:35:29 -0800

    Vehicle drive works now - had to switch vehicle mode to "Manual" and enable it first.

--- Assets/DeepRacer/ServoController.cs
+++ Assets/DeepRacer/ServoController.cs
@@ -5,18 +5,46 @@ using RosMessageTypes.DeepracerInterfacesPkg;
 public class ServoController : MonoBehaviour
 {
     private ROSConnection ros;
-    private string servoTopic = "ctrl_pkg/servo_msg";
+    private string manualDriveTopic = "webserver_pkg/manual_drive";
+    private string vehicleStateService = "ctrl_pkg/vehicle_state";
+    private string enableStateService = "ctrl_pkg/enable_state";
 
     public float publishFrequency = 0.5f;
     private float timeElapsed = 0.0f;
+
+    // Enums from https://github.com/aws-deepracer/aws-deepracer-ctrl-pkg/blob/main/ctrl_pkg/src/ctrl_node.cpp
+    enum CtrState
+    {
+        manual,
+        autonomous,
+        calibration,
+        numStates
+    }
 
     void Start()
     {
         // Grab the ROS Connection singleton
         ros = ROSConnection.GetOrCreateInstance();
 
-        // Register the servo publisher
-        ros.RegisterPublisher<ServoCtrlMsgMsg>(servoTopic);
+        // Register the servo publisher and the outbound service calls we plan to make
+        ros.RegisterPublisher<ServoCtrlMsgMsg>(manualDriveTopic);
+        ros.RegisterRosService<ActiveStateSrvRequest, ActiveStateSrvResponse>(vehicleStateService);
+        ros.RegisterRosService<EnableStateSrvRequest, EnableStateSrvResponse>(enableStateService);
+
+        // Set the vehicle mode to "Manual Drive"
+        ActiveStateSrvRequest vehicleStateSrvRequest = new ActiveStateSrvRequest((sbyte)CtrState.manual);
+        ros.SendServiceMessage<ActiveStateSrvResponse>(vehicleStateService, vehicleStateSrvRequest);
+
+        // Enable the current vehicle mode
+        EnableStateSrvRequest enableStateSrvRequest = new EnableStateSrvRequest(true);
+        ros.SendServiceMessage<EnableStateSrvResponse>(enableStateService, enableStateSrvRequest);
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("Deactivating Manual Drive Mode");
+        EnableStateSrvRequest enableStateSrvRequest = new EnableStateSrvRequest(false);
+        ros.SendServiceMessage<EnableStateSrvResponse>(enableStateService, enableStateSrvRequest);
     }
 
     void Update()
@@ -29,7 +57,8 @@ public class ServoController : MonoBehaviour
             Vector2 thumbstickInput = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
             Debug.Log("Thumbstick input: x=" + thumbstickInput.x + ", y=" + thumbstickInput.y);
             ServoCtrlMsgMsg servoCtrlMsg = new ServoCtrlMsgMsg(thumbstickInput.y, thumbstickInput.x);
-            ros.Publish(servoTopic, servoCtrlMsg);
+            //ServoCtrlMsgMsg servoCtrlMsg = new ServoCtrlMsgMsg(thumbstickInput.y, 0.8f);
+            ros.Publish(manualDriveTopic, servoCtrlMsg);
 
             timeElapsed = 0.0f;
         }
